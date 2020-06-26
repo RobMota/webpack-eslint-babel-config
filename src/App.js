d85537b4a00799474ba5f5701aec6450c9719763
RobMota <xaparxapando@gmail.com>
2020-06-26 15:41:50 -0300

    config webdev

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <>
+      <p>Primeiro Paragrafo</p>
+      <p>Segundo Paragrafo</p>
+      <Button />
+    </>
+  );
+};
+
+export default App;
